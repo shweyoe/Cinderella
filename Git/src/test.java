58ca0734675c3acb9b529d36398557c4e24fc402
Lucky <Lucky@gmail.com>
2016-08-24 12:32:05 +0700

    First check

---
+++ Git/src/test.java
@@ -0,0 +1,14 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int total=0;
+		for(int i=1;i<5;i++)
+		{
+			total=total+i;
+		}
+		System.out.print(total);
+	}
+
+}
